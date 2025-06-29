abe09ad96d6b888e07a311aaeadf15e72f3f0bb7
MatKos_169 <haun.m@outlook.de>
2025-06-29 12:33:03 +0200

    feat: added env variables to specify base outputfodler feat: output will end up in env:BASE_OUTPUT/OUTPUTFOLDER-{file creation year}/{file creation Month}/video.mp4

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,37 +1,45 @@
 #!/bin/sh
 
-# Exit immediately if a command exits with a non-zero status
 set -e
 
-# Set default values if environment variables are not provided
-: "${WORKDIR:=/app/downloads}"  # Define the working directory
-: "${SLEEPTIME:=600}"  # Define the SLEEPTIME in seconds
+: "${WORKDIR:=/app/downloads}"
+: "${SLEEPTIME:=600}"
+: "${BASE_OUTPUT:=/app/output}"
+: "${OUTPUTFOLDER:=streamername}"
 
 convert_ts_to_mp4() {
-    echo "Starting conversion of .ts files to .mp4 in ${WORKDIR}..."
+    processed_dir="${WORKDIR}/processed"
+    mkdir -p "${processed_dir}"
+    echo "Starting conversion of .ts files in ${WORKDIR}..."
 
-    # Find all .ts files in the WORKDIR
-    find "${WORKDIR}" -type f -name "*.ts" | while read -r ts_file; do
-        # Define the output .mp4 file path
-        mp4_file="${ts_file%.ts}.mp4"
+    find "${WORKDIR}" -maxdepth 1 -type f -name "*.ts" | while read -r ts_file; do
+        year=$(stat -c %y "${ts_file}" | cut -d- -f1)
+        month=$(stat -c %y "${ts_file}" | cut -d- -f2)
+        
+        base_folder="${BASE_OUTPUT}/${OUTPUTFOLDER}-${year}/${month}"
+        mkdir -p "${base_folder}"
 
+        mp4_file="${base_folder}/$(basename "${ts_file%.ts}").mp4"
+        
         echo "Converting '${ts_file}' to '${mp4_file}'..."
 
-        # Perform the conversion using ffmpeg
-        ffmpeg -i "${ts_file}" -c copy "${mp4_file}" -y
+        ffmpeg -i "${ts_file}" \
+          -hide_banner -loglevel error \
+          -map 0:v -map 0:a? \
+          -af "loudnorm=I=-14:TP=-1.5:LRA=11:measured_I=-33.0:measured_TP=-4.9:measured_LRA=19.3:measured_thresh=-45.4:offset=0.3:linear=true:print_format=summary" \
+          -c:v copy -c:a aac -b:a 192k \
+          "${mp4_file}"
 
         if [ $? -eq 0 ]; then
-            echo "Successfully converted '${ts_file}' to '${mp4_file}'."
-            rm "${ts_file}"
+            echo "Successfully converted to '${mp4_file}'."
+            echo "Moving original .ts file to processed directory..."
+            mv "${ts_file}" "${processed_dir}/"
         else
             echo "Failed to convert '${ts_file}'."
         fi
     done
-
-    echo "Conversion process completed."
 }
 
-# Convert .ts files to .mp4 after downloading
 convert_ts_to_mp4
 
 echo "All tasks completed successfully. Sleeping for $SLEEPTIME seconds..."
